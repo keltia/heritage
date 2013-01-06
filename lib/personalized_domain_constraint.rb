f8410625865d0d383dab6137f7c67bd6e086deb8
Fabien Penso <fabienpenso@gmail.com>
2013-01-06 22:21:41 +0100

    fixing bugs for fabienpenso.com

--- lib/personalized_domain_constraint.rb
+++ lib/personalized_domain_constraint.rb
@@ -4,7 +4,10 @@ class PersonalizedDomainConstraint
   end
 
   def matches?(request)
-    User.count(:conditions => {:specific_url => request.server_name}) > 0 ||
+    host_for_query = request.server_name
+    host_for_query = "www.#{host_for_query}" if host_for_query !~ /^www\./
+    
+    User.count(:conditions => {:specific_url => host_for_query}) > 0 ||
       User.count(:conditions => {:internal_url => request.server_name}) > 0
   end
 end
