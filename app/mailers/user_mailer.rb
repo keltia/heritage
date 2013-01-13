80052ebd1ab05f3d4b34329adf0d92dc46fbd55d
Fabien Penso <fabienpenso@gmail.com>
2013-01-13 21:31:14 +0100

    Fixing email froms

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: Settings[:email_from]
+  default :from => Settings[:email_from],
+    :reply_to => Settings[:email_reply_to]
 
   def welcome_email(user)
     @user = user
-    mail(:to => user.email, :subject => "Welcome to Heritage")
+    mail(:to => user.email, 
+         :subject => "Welcome to Heritage")
   end
 end
