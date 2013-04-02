c0709e8bc62ca1ee384d87dd2a914238d78c0a3e
Fabien Penso <fabienpenso@gmail.com>
2013-04-03 00:52:20 +0200

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,12 @@ jQuery(function($) {
 		$("#new_email").bind('ajax:before', function() {
       $('#new_email_submit').val('Loading...');
 		});
+
+    $(".imgHover").hover(
+      function() {
+      $(this).children("img").fadeTo(200, 0.25).end().children(".hover").show();
+    },
+    function() {
+      $(this).children("img").fadeTo(200, 1).end().children(".hover").hide();
+    });
 })
